52427e66814ef26e25716a276e24702ca52bcaad
Batuhan Karakaya <91139544+Batuhank8931@users.noreply.github.com>
2025-07-23 13:05:50 +0300

    Update open_url.py

--- script/open_url.py
+++ script/open_url.py
@@ -5,4 +5,11 @@ with open('/home/user/Desktop/script/url.txt', 'r') as file:
     url = file.read().strip()  # Read and strip any extra whitespace
 
 # Open the URL in Chromium in full-screen mode
-subprocess.run(['chromium', '--start-fullscreen', url])
+subprocess.run([
+    'chromium',
+    '--noerrdialogs',
+    '--disable-session-crashed-bubble',
+    '--disable-infobars',
+    '--start-fullscreen',
+    url
+])
